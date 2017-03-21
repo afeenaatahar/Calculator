47bddd659135a8422ced64db89c87f803ec3af55
Afeena Atahar <aatahar@nisum.com>
2017-03-21 09:38:11 -0300

    this is my first commit to repository

---
+++ src/exercise1/Main.java
@@ -0,0 +1,12 @@
+package exercise1;
+
+public class Main {
+	public static void main(String[] args){
+		Calculation casio=new Calculator();
+		int c=casio.add(7,11);
+		System.out.println(""+c);
+		
+	}
+	
+
+}
